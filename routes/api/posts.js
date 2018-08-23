cf333a321336a3dae96244850be870abc39d09ed
Leandra <leandra.figueira@gmail.com>
2018-08-23 12:54:24 -0500

    End of section 4 of the training.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+const Post = require("../../models/Post");
 
 // @route  GET api/posts/test
 // @desc   Tests Posts route
 // @access Public
 router.get("/test", (req, res) => res.json({ msg: "Posts works" }));
+
+// @route  POST api/posts
+// @desc   Create Post
+// @access Private
+router.post(
+    "/",
+    passport.authenticate("jwt", { session: false }), (req, res) => {
+        const newPost = new Post({
+            text:
+        })
+    });
 
 module.exports = router;
